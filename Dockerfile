d75e84943801ca3af84c6df513ab2ed98f4f250b
Andrew M Melnick <andrew.m.melnick@aero.org>
2019-02-07 12:07:12 -0500

    Update to new version of jetty

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM jetty:9.3.11-jre8-alpine
+FROM jetty:9.4.12-jre8-alpine
 
 # These are defaults, change them
 ENV SSO_AUTH_MYSQL_URL=localhost
@@ -8,19 +8,17 @@ ENV SSO_AUTH_MYSQL_PASSWORD=password
 # Password for the certificate keystore
 ENV JETTY_KEYSTORE_PASSWORD=CHANGEME
 
-# The relative path from $JETTY_HOME/etc/keystore_mount your keystore is placed in
-ENV JETTY_KEYSTORE_RELATIVE_PATH=keystore
-
-# Mount the keystore here
-VOLUME $JETTY_HOME/etc/keystore_mount
-
-WORKDIR $JETTY_HOME
+# If set to a non-empty string, the keystore will be imported into the trust
+# store. This is necessary if your SSL certificates are not trusted by java
+# by default, i.e. self-signed
+ENV TRUST_KEYSTORE=
 
 # Copy in war file and associated configuration xml
-COPY accounts.xml "$JETTY_HOME/webapps/accounts.xml"
+COPY accounts.xml "$JETTY_BASE/webapps/accounts.xml"
 COPY target/accounts.war /opt/accounts.war
 
 # Add in new entrypoint script
+USER root
 RUN mv /docker-entrypoint.sh /jetty-docker-entrypoint.sh
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,32 @@
 #!/bin/sh -xe
 
-KEYSTORE_MOUNT=$JETTY_HOME/etc/keystore_mount
 UTIL_JAR=$JETTY_HOME/lib/jetty-util-*.jar
 PASSWORD_CLASS=org.eclipse.jetty.util.security.Password
-START_INI="$JETTY_HOME/start.ini"
-DEFAULT_KEYSTORE_PATH=etc/keystore
-NEW_KEYSTORE_PATH=$KEYSTORE_MOUNT/$JETTY_KEYSTORE_RELATIVE_PATH
-#KEYSTORE_PATH_REPLACER='s|$DEFAULT_KEYSTORE_PATH|$NEW_KEYSTORE_PATH|g'
+START_INI="$JETTY_BASE/start.d/server.ini"
+JETTY_KEYSTORE_PATH=$JETTY_BASE/etc/keystore
 OBF_PASSWORD=$(java -cp $UTIL_JAR $PASSWORD_CLASS $JETTY_KEYSTORE_PASSWORD 2>&1 | grep OBF)
 
-echo '--module=https' >> "$JETTY_HOME/start.ini"
-echo '--module=ssl' >> "$JETTY_HOME/start.ini"
-echo '--module=resources' >> "$JETTY_HOME/start.ini"
+echo '--module=https' >> $START_INI
+echo '--module=ssl' >> $START_INI
+#echo '--module=logging-log4j2' >> $START_INI
 
 ([ "$OBF_PASSWORD" ] || (echo "No password provided" && exit -1))
 echo jetty.truststore.password=$OBF_PASSWORD >> $START_INI
 echo jetty.keystore.password=$OBF_PASSWORD >> $START_INI
 echo jetty.keymanager.password=$OBF_PASSWORD >> $START_INI
-ln -s $NEW_KEYSTORE_PATH $DEFAULT_KEYSTORE_PATH
 
-/jetty-docker-entrypoint.sh $@
+
+
+if [ -n "$TRUST_KEYSTORE" ]; then
+    DEFAULT_JAVA_TRUSTSTORE_PATH=/etc/ssl/certs/java/cacerts
+    DEFAULT_JAVA_TRUSTSTORE_PASSWORD=changeit
+    
+    keytool -importkeystore \
+        -srckeystore $JETTY_KEYSTORE_PATH \
+        -srcstorepass $JETTY_KEYSTORE_PASSWORD \
+        -destkeystore $DEFAULT_JAVA_TRUSTSTORE_PATH \
+        -deststorepass $DEFAULT_JAVA_TRUSTSTORE_PASSWORD \
+
+fi
+
+su -s /bin/sh jetty -- /jetty-docker-entrypoint.sh $@
